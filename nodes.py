8667f5e4de64bdc9cdd9459dd01b13709c1d31d9
Alina Dima <alina.dima@tum.de>
2025-03-17 14:56:07 +0100

    Updated node parsing to conform to the newly installed slurm version, which includes gpumem specification.

--- nodes.py
+++ nodes.py
@@ -28,7 +28,7 @@ class Node:
     def __repr__(self):
         return (
             f"Name: {self.name}".ljust(25)
-            + f"GPUs: {self.gpu_count}({self.gpu_type})".ljust(30)
+            + f"GPUs: {self.gpu_count}({self.gpu_type})".ljust(50)
             + f"State: {self.state}".ljust(20)
             + ("asteroid" if self.is_asteroid else "universe").ljust(10)
             + ("private" if self.is_private else "public").ljust(10)
@@ -38,37 +38,73 @@ class Node:
 def parse_complex_gpu_descriptions(gpu_raw, node_name):
     """
     Parses gpu info. Can handle nodes that contain multiple GPU types.
-    gpu_raw is obtained from sinfo
-    Example string from a node with 2 gpu types:
-    'gpu:RTX8000:2,gpu:RTX6000:2'
+    gpu_raw is obtained from sinfo, and it contains a list of 
+    GPU specs (gpu:) followed by a list of corresponding memory specs (gpumem:).
+    Example string from a node with 2 gpu types (name1 and name2) with 2 GPUs each:
+    'gpu:name1:2(S:0-15),gpu:name2:2(S:0-15),gpumem:name1:48G,gpumem:name2:48G'
 
     Args:
         gpu_raw (str): The raw string containing the GPU info
         node_name (str): The name of the node
 
     Raises:
-        NotImplementedError: In case the info does not match the expected format.
+        ValueError: In case the info does not match the expected format.
 
     Returns:
         Tuple(int, str): Total number of nodes and their type
     """
-    gres_regex = r"gpu:(\w*):(\d)(\(?.*\)?)"
+    # Step 1: Parse the gpu_raw string into a list of entities corresponding 
+    # to each gpu type its respective memory spec (2 entities per gpu type)
 
+    # The entities are separated by commas, but the socket info can also contain commas
+    # 1a. Split by gpu: and gpumem:
+    entities = re.split(r"gpu:|gpumem:", gpu_raw)[1:]
+    # 1b. Remove final comma if present
+    entities = [e[:-1] if e[-1] == "," else e for e in entities]
+    # 1c. Check if the number of entities is even
+    if len(entities) % 2 == 1:
+        raise ValueError(
+            f"Node GPU parsing error: Odd number of entities: {len(entities)}"
+        )
+    T = len(entities) // 2
+    # 1d. Double check the format by adding "gpu:" and "gpumem:" back to the entities
+    recon_gpu_raw = ",".join(
+        ["gpu:" + e for e in entities[:T]]
+        + ["gpumem:" + e for e in entities[T:]]
+    )
+    if recon_gpu_raw != gpu_raw:
+        raise ValueError(
+            "Node GPU parsing error: "
+            + f"Expected format: {recon_gpu_raw}"
+            + f"Got format: {gpu_raw}"
+        )
+
+    # Step 2: Parse the entities into a list of tuples containing 
+    # the gpu type, count, and memory
     individual_gpus = []
-    for current_gpu_raw in gpu_raw.split(","):
-        matches = re.match(gres_regex, current_gpu_raw)
-        if not matches:
-            raise NotImplementedError(
-                f"Could not parse GPU info for node {node_name}: "
-                + f"{current_gpu_raw}"
-                "Please check the sinfo output."
-            )
-        else:
-            gpu_type, gpu_count, _ = matches.groups()
-            individual_gpus.append((gpu_type, int(gpu_count)))
+    for gpu_type_raw, gpu_mem_raw in zip(entities[:T], entities[T:]):
+        
+        # 2a. Parse the gpu name and count using regex
+        gres_regex = r"(\w*):(\d)(\(?.*\)?)"
+        gres_match = re.match(gres_regex, gpu_type_raw)
+        gpu_count = int(gres_match.groups()[1])
+        gpu_type1 = gres_match.groups()[0]
 
-    gpu_count = sum([count for _, count in individual_gpus])
-    gpu_type = ",".join([f"{y}x{x}" for x, y in individual_gpus])
+        # 2b. Parse the memory using regular split
+        gpu_type2, gmem = gpu_mem_raw.split(":")
+
+        # 2c. Check if the gpu type is the same
+        if not gpu_type2 == gpu_type1:
+            raise ValueError(
+                f"Parsing error @{node_name}: GPU type mismatch {gpu_type1} != {gpu_type2}"
+            )
+
+        # 2d. Append the gpu type, count, and memory to the list
+        individual_gpus.append((gpu_type1, gpu_count, gmem))
+
+    # Step 3: Sum the gpu counts and format the gpu type
+    gpu_count = sum([count for _, count, _ in individual_gpus])
+    gpu_type = ",".join([f"{y}x{x}({m})" for x, y, m in individual_gpus])
     return gpu_count, gpu_type
 
 
